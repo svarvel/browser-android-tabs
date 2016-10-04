17a361f7e0784799c388bae690e71f5e11e00ce9
Serg <serg.zhukovsky@gmail.com>
2016-10-04 13:46:17 -0400

    Added Block 3rd party cookies switch
    Auditors: @bbondy
    
    Conflicts:
    	chrome/android/java/src/org/chromium/chrome/browser/init/ChromeBrowserInitializer.java
    	components/content_settings/core/browser/cookie_settings.cc
    	components/content_settings/core/browser/cookie_settings.h

--- chrome/android/java/src/org/chromium/chrome/browser/init/ChromeBrowserInitializer.java
+++ chrome/android/java/src/org/chromium/chrome/browser/init/ChromeBrowserInitializer.java
@@ -37,6 +37,7 @@ import org.chromium.chrome.browser.ChromeSwitches;
 import org.chromium.chrome.browser.FileProviderHelper;
 import org.chromium.chrome.browser.crash.LogcatExtractionRunnable;
 import org.chromium.chrome.browser.download.DownloadManagerService;
+import org.chromium.chrome.browser.preferences.PrefServiceBridge;
 import org.chromium.chrome.browser.services.GoogleServicesManager;
 import org.chromium.chrome.browser.tabmodel.document.DocumentTabModelImpl;
 import org.chromium.chrome.browser.webapps.ActivityAssigner;
@@ -119,6 +120,7 @@ public class ChromeBrowserInitializer {
       new DownloadTrackingProtectionDataAsyncTask().execute();
       new DownloadAdBlockDataAsyncTask().execute();
       new DownloadHTTPSDataAsyncTask().execute();
+      PrefServiceBridge.getInstance().setBlockThirdPartyCookiesEnabled(true);
     }
 
     // Tracking ptotection data download
