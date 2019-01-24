88479ba088b43aa6978eb417b822d912437b5f6d
George Dregalo <37560650+gdregalo@users.noreply.github.com>
2019-01-24 16:41:04 -0500

    reopen existing rewards page if exists

--- chrome/android/java/src/org/chromium/chrome/browser/BraveRewardsPanelPopup.java
+++ chrome/android/java/src/org/chromium/chrome/browser/BraveRewardsPanelPopup.java
@@ -187,7 +187,7 @@ public class BraveRewardsPanelPopup implements BraveRewardsObserver, FaviconHelp
                 int firstOffset = text.indexOf(". ");
                 if (offset > firstOffset) {
                     // We are on learn more
-                    launchTabInRunningTabbedActivity(new LoadUrlParams(ChromeTabbedActivity.REWARDS_LEARN_MORE_URL));
+                    mActivity.openNewOrSelectExistingTab (ChromeTabbedActivity.REWARDS_LEARN_MORE_URL);
                     dismiss();
                 }
             }
@@ -220,7 +220,7 @@ public class BraveRewardsPanelPopup implements BraveRewardsObserver, FaviconHelp
           tvLearnMore.setOnClickListener((new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                launchTabInRunningTabbedActivity(new LoadUrlParams(ChromeTabbedActivity.REWARDS_SETTINGS_URL));
+                mActivity.openNewOrSelectExistingTab (ChromeTabbedActivity.REWARDS_SETTINGS_URL);
                 dismiss();
             }
           }));
@@ -231,7 +231,7 @@ public class BraveRewardsPanelPopup implements BraveRewardsObserver, FaviconHelp
           btAddFunds.setOnClickListener((new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                launchTabInRunningTabbedActivity(new LoadUrlParams(ChromeTabbedActivity.ADD_FUNDS_URL));
+                mActivity.openNewOrSelectExistingTab (ChromeTabbedActivity.ADD_FUNDS_URL);
                 dismiss();
             }
           }));
@@ -241,7 +241,7 @@ public class BraveRewardsPanelPopup implements BraveRewardsObserver, FaviconHelp
             btRewardsSettings.setOnClickListener((new View.OnClickListener() {
               @Override
               public void onClick(View v) {
-                  launchTabInRunningTabbedActivity(new LoadUrlParams(ChromeTabbedActivity.REWARDS_SETTINGS_URL));
+                  mActivity.openNewOrSelectExistingTab (ChromeTabbedActivity.REWARDS_SETTINGS_URL);
                   dismiss();
               }
             }));
@@ -354,11 +354,11 @@ public class BraveRewardsPanelPopup implements BraveRewardsObserver, FaviconHelp
                 int secondOffset = text.indexOf(". ");
                 if (offset > firstOffset) {
                     // We are on change auto-contribute settings
-                    launchTabInRunningTabbedActivity(new LoadUrlParams(ChromeTabbedActivity.REWARDS_SETTINGS_URL));
+                    mActivity.openNewOrSelectExistingTab (ChromeTabbedActivity.REWARDS_SETTINGS_URL);
                     dismiss();
                 } else if (offset <= firstOffset && offset > secondOffset) {
                     // We are on learn more
-                    launchTabInRunningTabbedActivity(new LoadUrlParams(ChromeTabbedActivity.REWARDS_LEARN_MORE_URL));
+                    mActivity.openNewOrSelectExistingTab (ChromeTabbedActivity.REWARDS_LEARN_MORE_URL);
                     dismiss();
                 }
             }
