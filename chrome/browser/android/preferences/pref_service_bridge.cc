remzert/BraveBrowser

AdBlock, tracking protection, h264, aac support
Auditors: @bbondy

chrome/browser/android/preferences/pref_service_bridge.cc
@@ -351,6 +351,18 @@ static void SetSafeBrowsingEnabled(JNIEnv* env,
   GetPrefService()->SetBoolean(prefs::kSafeBrowsingEnabled, enabled);
 }
 
+static void SetAdBlockEnabled(JNIEnv* env,
+                                   const JavaParamRef<jobject>& obj,
+                                   jboolean enabled) {
+   GetPrefService()->SetBoolean(prefs::kAdBlockEnabled, enabled);
+}
+
+static void SetTrackingProtectionEnabled(JNIEnv* env,
+                                   const JavaParamRef<jobject>& obj,
+                                   jboolean enabled) {
+   GetPrefService()->SetBoolean(prefs::kTrackingProtectionEnabled, enabled);
+}
+
 static jboolean GetSafeBrowsingManaged(JNIEnv* env,
                                        const JavaParamRef<jobject>& obj) {
   return GetPrefService()->IsManagedPreference(prefs::kSafeBrowsingEnabled);

chrome/browser/profiles/profile_io_data.cc
@@ -476,6 +476,8 @@ void ProfileIOData::InitializeOnUIThread(Profile* profile) {
   ChromeNetworkDelegate::InitializePrefsOnUIThread(
       &enable_referrers_,
       &enable_do_not_track_,
+      &enable_tracking_protection_,
+      &enable_ad_block_,
       &force_google_safesearch_,
       &force_youtube_safety_mode_,
       &allowed_domains_for_apps_,
@@ -1066,6 +1068,8 @@ void ProfileIOData::Init(
   network_delegate->set_profile_path(profile_params_->path);
   network_delegate->set_cookie_settings(profile_params_->cookie_settings.get());
   network_delegate->set_enable_do_not_track(&enable_do_not_track_);
+  network_delegate->set_enable_tracking_protection(&enable_tracking_protection_);
+  network_delegate->set_enable_ad_block(&enable_ad_block_);
   network_delegate->set_force_google_safe_search(&force_google_safesearch_);
   network_delegate->set_force_youtube_safety_mode(&force_youtube_safety_mode_);
   network_delegate->set_allowed_domains_for_apps(&allowed_domains_for_apps_);
@@ -1284,6 +1288,8 @@ void ProfileIOData::ShutdownOnUIThread(
   google_services_user_account_id_.Destroy();
   enable_referrers_.Destroy();
   enable_do_not_track_.Destroy();
+  enable_tracking_protection_.Destroy();
+  enable_ad_block_.Destroy();
   force_google_safesearch_.Destroy();
   force_youtube_safety_mode_.Destroy();
   allowed_domains_for_apps_.Destroy();

svarvel/browser-android-tabs

Conflicts:
	chrome/android/BUILD.gn
	out/Default/obj/chrome/android/chrome_public_apk_assets__build_config.ninja
	out/Default/obj/chrome/android/chrome_public_apk_manifest.ninja
	out/Default/obj/chrome/android/chrome_public_apk_resources__build_config.ninja
	out/Default/obj/chrome/android/chrome_public_apk_resources__process_resources.ninja
	out/Default/obj/chrome/android/chrome_public_apk_template_resources__template.ninja
	out/Default/obj/chrome/android/chrome_public_test_apk_manifest.ninja
	out/Default/obj/chrome/browser/browser.ninja
	chrome/android/chrome_apk.gyp
	chrome/browser/net/chrome_network_delegate.cc
	chrome/chrome_android_paks.gypi
	chrome/chrome_browser.gypi
	media/base/key_systems.cc
	chrome/android/java/src/org/chromium/chrome/browser/preferences/PrefServiceBridge.java
	chrome/android/java/src/org/chromium/chrome/browser/preferences/privacy/PrivacyPreferences.java
	chrome/android/java/strings/android_chrome_strings.grd
	chrome/common/pref_names.cc
	chrome/common/pref_names.h

chrome/browser/android/preferences/pref_service_bridge.cc
@@ -443,6 +443,18 @@ static void JNI_PrefServiceBridge_SetSafeBrowsingEnabled(
   GetPrefService()->SetBoolean(prefs::kSafeBrowsingEnabled, enabled);
 }
 
+static void JNI_PrefServiceBridge_SetAdBlockEnabled(JNIEnv* env,
+                                   const JavaParamRef<jobject>& obj,
+                                   jboolean enabled) {
+   GetPrefService()->SetBoolean(prefs::kAdBlockEnabled, enabled);
+}
+
+static void JNI_PrefServiceBridge_SetTrackingProtectionEnabled(JNIEnv* env,
+                                   const JavaParamRef<jobject>& obj,
+                                   jboolean enabled) {
+   GetPrefService()->SetBoolean(prefs::kTrackingProtectionEnabled, enabled);
+}
+
 static jboolean JNI_PrefServiceBridge_GetSafeBrowsingManaged(
     JNIEnv* env,
     const JavaParamRef<jobject>& obj) {

chrome/browser/profiles/profile_io_data.cc
@@ -437,6 +437,11 @@ void ProfileIOData::InitializeOnUIThread(Profile* profile) {
   allowed_domains_for_apps_.MoveToThread(
       base::CreateSingleThreadTaskRunnerWithTraits({BrowserThread::IO}));
 
+  ChromeNetworkDelegate::InitializePrefsOnUIThread(
+      &enable_tracking_protection_,
+      &enable_ad_block_
+      pref_service);
+
   scoped_refptr<base::SingleThreadTaskRunner> io_task_runner =
       base::CreateSingleThreadTaskRunnerWithTraits({BrowserThread::IO});
 
@@ -944,6 +949,8 @@ void ProfileIOData::Init(
         profile_params_->cookie_settings.get());
     chrome_network_delegate->set_force_google_safe_search(
         &force_google_safesearch_);
+    chrome_network_delegate->set_enable_tracking_protection(&enable_tracking_protection_);
+    chrome_network_delegate->set_enable_ad_block(&enable_ad_block_);
 
     chrome_network_delegate_unowned_ = chrome_network_delegate.get();
 
@@ -1159,6 +1166,8 @@ void ProfileIOData::ShutdownOnUIThread(
 #if !defined(OS_CHROMEOS)
   signin_scoped_device_id_.Destroy();
 #endif
+  enable_tracking_protection_.Destroy();
+  enable_ad_block_.Destroy();
   force_google_safesearch_.Destroy();
   force_youtube_restrict_.Destroy();
   allowed_domains_for_apps_.Destroy();