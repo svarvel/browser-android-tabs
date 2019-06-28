2d4b85c1c5ac4861fb8274e76db0cf3ad30eccf2
Terry Mancey <terry.mancey@icloud.com>
2019-06-28 09:54:00 +0100

    Implement Android Ads

--- chrome/android/java/src/org/chromium/chrome/browser/init/ChromeBrowserInitializer.java
+++ chrome/android/java/src/org/chromium/chrome/browser/init/ChromeBrowserInitializer.java
@@ -173,10 +173,6 @@ public class ChromeBrowserInitializer {
       new SwitchSearchSuggestAsyncTask().executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
     }
 
-    private void NotifyOnFirstRun(){
-        BraveSetDefaultBrowserNotificationService.NotifyRewardsLive();
-    }
-
     // ADBlock update
     class UpdateADBlockAsyncTask extends AsyncTask<Long> {
         @Override
@@ -596,7 +592,6 @@ public class ChromeBrowserInitializer {
         UpdateStats();
         CheckInstallationSource();
         SwitchSearchSuggestEnabled();
-        NotifyOnFirstRun();
     }
 
     private ActivityStateListener createActivityStateListener() {
