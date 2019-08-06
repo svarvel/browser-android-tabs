d6a1c6f05f090bbae5c2962f1d466f02682a9938
svarvel <varvello@gmail.com>
2019-08-06 16:49:33 -0400

    reading file with proto info (debug)

--- net/http/http_network_session.cc
+++ net/http/http_network_session.cc
@@ -280,8 +280,11 @@ HttpNetworkSession::HttpNetworkSession(const Params& params,
   file_to_read.close();
 
   // enable H2 if possible 
-  if (params_.enable_http2 and protocol_to_use == "H2")
+  if (params_.enable_http2 && protocol_to_use.compare("H2") == 0){
+  //if (params_.enable_http2 && protocol_to_use == "H2"){
+    std::string::compare()
     next_protos_.push_back(kProtoHTTP2);
+  }
   next_protos_.push_back(kProtoHTTP11);
 
   http_server_properties_->SetMaxServerConfigsStoredInProperties(
